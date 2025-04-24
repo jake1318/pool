7da93ae1d0c295255983822f5a3e036f729ad20a
jake1318 <106035392+jake1318@users.noreply.github.com>
2025-04-24 13:15:28 -0400

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@mysten/sui": path.resolve(
+        __dirname,
+        "node_modules/@mysten/sui/dist/index.js"
+      ),
+    },
+  },
+});
